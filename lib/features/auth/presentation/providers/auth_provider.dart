3c6931a76d337a3034310ea92a51878296954a0a
Max Vigueras <mvt.2000@hotmail.com>
2024-02-29 21:17:27 -0600

    Auth: Save token on device

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,20 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
 
   final authRepository = AuthRepositoryImpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
 
-  return AuthNotifier(authRepository: authRepository); 
+  return AuthNotifier(
+    authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService
+  ); 
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
 
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
 
   AuthNotifier({
-    required this.authRepository
+    required this.authRepository,
+    required this.keyValueStorageService
     })
     : super(AuthState());
 
@@ -36,8 +44,10 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   void checkAuthStatus () async{}
 
-  void _setLoggedUser(User user){
-    //TODO: necesito guardar el token fisicamente
+  void _setLoggedUser(User user) async{
+
+    await keyValueStorageService.setKeyValue('token', user.token);
+
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -46,7 +56,9 @@ class AuthNotifier extends StateNotifier<AuthState> {
   }
 
   Future <void> logout([String? errorMessage]) async{
-    //TODO: Limpiar token
+
+    await keyValueStorageService.removeKey('token');
+
     state = state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
