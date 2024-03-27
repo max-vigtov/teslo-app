ce594321279ed829647a7582bf30651d478dfc8d
Max Vigueras <mvt.2000@hotmail.com>
2024-03-26 18:59:26 -0600

    Products: Products provider created

---
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -0,0 +1,34 @@
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsState{
+
+  final bool isLastPage;
+  final int limit;
+  final int offset;
+  final bool isLoading;
+  final List<Product> products;
+
+  ProductsState({
+    this.isLastPage = false,
+    this.limit = 10,
+    this.offset = 0,
+    this.isLoading = false,
+    this.products = const[]
+  });
+
+
+ProductsState copyWith(
+  bool? isLastPage,
+  int? limit,
+  int? offset,
+  bool? isLoading,
+  List<Product>? products,  
+ ) => ProductsState(
+  isLastPage: isLastPage ?? this.isLastPage,
+  limit: limit ?? this.limit,
+  offset: offset ?? this.offset,
+  isLoading: isLoading ?? this.isLoading,
+  products: products ?? this.products,
+
+ );
+}
