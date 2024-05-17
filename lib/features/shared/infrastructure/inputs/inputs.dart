b88a1afa502ca15caa7d0dc02c9ff2c9d83634fb
Max Vigueras <mvt.2000@hotmail.com>
2024-05-16 18:54:52 -0600

    Product: Edit product screen design added

--- lib/features/shared/infrastructure/inputs/inputs.dart
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -1,2 +1,6 @@
+export 'price.dart';
+export 'slug.dart';
+export 'stock.dart';
+export 'title.dart';
 export 'email.dart';
 export 'password.dart';
--- lib/features/shared/widgets/widgets.dart
+++ lib/features/shared/widgets/widgets.dart
@@ -1,4 +1,7 @@
 
+export 'package:teslo_shop/features/shared/widgets/custom_product_field.dart';
+
+export 'package:teslo_shop/features/shared/widgets/full_screen_loader.dart';
 export 'custom_filled_button.dart';
 export 'custom_text_form_field.dart';
 export 'geometrical_background.dart';
